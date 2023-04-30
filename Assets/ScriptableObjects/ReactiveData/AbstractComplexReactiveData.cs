3bbb7f3e7c509a31dbe4ac61d35942362b502339
Михайло Торольчук <torolchuk@MacBook-Pro-Mihajlo.local>
2023-04-30 17:12:48 +0300

    complex reactive data and additional icons

---
+++ Assets/ScriptableObjects/ReactiveData/AbstractComplexReactiveData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace DeliveryDroneGame.Utils
+{
+    public abstract class AbstractComplexReactiveData<ValueType>
+    : ScriptableObject,
+        IReactiveValue<ValueType>,
+         ISubscribe
+    {
+        [SerializeField]
+        private ValueType _value;
+        private event Action listeners;
+
+        public ValueType GetValue()
+        {
+            return _value;
+        }
+
+        public void SetValue(ValueType value)
+        {
+            _value = value;
+            if (listeners != null)
+                listeners();
+        }
+
+        public void Subscribe(Action callback)
+        {
+            listeners += callback;
+        }
+
+        public void Unsubscribe(Action callback)
+        {
+            listeners -= callback;
+        }
+    }
+}
---
+++ Assets/ScriptableObjects/ReactiveData/ReactivePickupItemList.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using DeliveryDroneGame.Utils;
+
+namespace DeliveryDroneGame.Utils
+{
+    [CreateAssetMenu(
+        fileName = "NewReactivePickupItemList",
+        menuName = "ScriptableObjects/ReactiveData/PickupItemList"
+    )]
+    public class ReactivePickupItemList : AbstractComplexReactiveData<List<PickupItemScriptableObject>>
+    {
+    }
+}
+
